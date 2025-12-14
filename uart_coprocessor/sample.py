971ff82b4500f1a9705816f831fc5ff3d6afea60
Terence Chan Zun Mun <zunmun@gmail.com>
2025-12-14 11:48:49 +0800

    feat: update circuitpython code

--- uart_coprocessor/sample.py
+++ uart_coprocessor/sample.py
@@ -12,22 +12,25 @@ if input("type something to update fpga: ") != "":
     h = hardware.fpga.upload_bitstream(PATH+"/coprocessor.bit")
     h.deinit()
 
-### Need clear Pins first #######################
-DATA_PINS_NO = [board.GP8, board.GP9, board.GP10, board.GP11, board.GP12]
-pins = []
-for p in DATA_PINS_NO:
-    d = digitalio.DigitalInOut(p)
-    d.direction = digitalio.Direction.OUTPUT
-    d.value = False
-    pins.append(d)
-            
-for d in pins:
-    d.deinit()
+
 
 ### Configure UART GPIO #########################
-class FpgaCoprocessor():
+class FpgaCoprocessor():    
+    def clear_pins(self):
+        ### Need clear Pins first #######################
+        DATA_PINS_NO = [board.GP8, board.GP9, board.GP10, board.GP11, board.GP12]
+        pins = []
+        for p in DATA_PINS_NO:
+            d = digitalio.DigitalInOut(p)
+            d.direction = digitalio.Direction.OUTPUT
+            d.value = False
+            pins.append(d)
+                    
+        for d in pins:
+            d.deinit()
     
     def __init__(self):
+        self.clear_pins()
         self.FRAME_SIZE = 16
         self.uart = busio.UART(board.GP8, board.GP9, baudrate=460800, timeout=0.1)
         DATA_PINS_NO = [board.GP10, board.GP11, board.GP12, board.GP13, board.GP14, board.GP15]
@@ -69,19 +72,20 @@ fp.pins[1].value = 0 # Forward mode
 
 fp.write_int(10)
 print(fp.read_int())
+fp.write_int(11)
+print(fp.read_int())
 
-### Previous mode
+### Previous (2nd) mode
 fp.pins[0].value = 0; fp.pins[1].value = 1
-fp.write_int(11)
+fp.write_int(12)
 print(fp.read_int()) # Should be 10
-fp.write_int(11)
+fp.write_int(13)
 print(fp.read_int()) # Should be 11
 
 ### Addition Mode
-fp.pins[0].value = 0; fp.pins[1].value = 0
+fp.pins[0].value = 0; fp.pins[1].value = 0;
 fp.write_int(11)
-print(fp.read_int()) # Should be 22
-fp.write_int(1000)
-print(fp.read_int()) # Should be 1011
-
+print(fp.read_int()) # 13 + 11 = 24
+fp.write_int(10000)
+print(fp.read_int()) # Should be 10011
 
